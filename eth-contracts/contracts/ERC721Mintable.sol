c3b369601154c9388da384bccb7fa63ffb56ab5f
TuanNS10 <sanhtuanst1029@gmail.com>
2022-07-13 11:47:49 +0700

    Update code to submit

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -547,12 +547,10 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 contract CustomERC721Token is ERC721Metadata{
     //  1) Pass in appropriate values for the inherited ERC721Metadata contract
     //      - make the base token uri: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/
-    constructor(string memory name, string memory symbol)
-    ERC721Metadata(name, symbol, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") 
-        public
-        {
+    constructor() public
+    ERC721Metadata("Sanh Tuan Token", "STtoken", "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") {
 
-        }
+    }
     //  2) create a public mint() that does the following:
     //      -can only be executed by the contract owner
     //      -takes in a 'to' address, tokenId, and tokenURI as parameters
